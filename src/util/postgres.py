d120e1cde101a490b723c53b65564df12f997ccc
Luka Žontar <luka.zontar.consulting@gmail.com>
2024-12-27 15:36:11 +0000

    New collaborations driven by search for expertise: preliminary results.

--- src/util/postgres.py
+++ src/util/postgres.py
@@ -1,7 +1,8 @@
+import polars as pl
 import pandas as pd
 import psycopg2
 import sqlalchemy
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Engine
 
 
 def create_connection(username: str,
@@ -58,6 +59,32 @@ def create_sqlalchemy_connection(username: str,
     return conn
 
 
+def create_sqlalchemy_engine(username: str,
+                             password: str,
+                             host: str,
+                             port: str,
+                             database: str,
+                             schema: str) -> Engine:
+    """
+    Create a connection to Postgres using SQLAlchemy
+    :param username: Postgres username
+    :param password: Postgres password
+    :param host: Postgres host
+    :param port: Postgres port
+    :param database: Postgres database
+    :param schema: Postgres schema
+    :return: SQLAlchemy connection
+    """
+    # Define the connection string
+    conn_string = f'postgresql://{username}:{password}@{host}:{port}/{database}'
+    # Create the connection
+    engine = create_engine(
+        conn_string,
+        connect_args={'options': '-csearch_path={}'.format(schema)})
+    # Return the connection
+    return engine
+
+
 def query(conn: psycopg2.extensions.connection | sqlalchemy.engine.base.Connection, query_str: str) -> pd.DataFrame:
     """
     Query Postgres.
@@ -67,6 +94,19 @@ def query(conn: psycopg2.extensions.connection | sqlalchemy.engine.base.Connecti
     """
     # Fetch the data
     df = pd.read_sql(query_str, conn)
+    # Return the DataFrame
+    return df
+
+
+def query_polars(conn: sqlalchemy.engine.base.Connection, query_str: str) -> pl.DataFrame:
+    """
+    Query Postgres.
+    :param conn: Postgres connection
+    :param query_str: SQL query
+    :return: Polars DataFrame with the data
+    """
+    # Fetch the data
+    df = pl.read_database(query_str, conn)
     # Return the DataFrame
     return df
 
